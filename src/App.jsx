d5263cd07a4b9e39384fc7d92d93cbc69aa49e29
Laionel2002 <lionel.a.gomez.g@gmail.com>
2025-01-29 00:00:59 -0300

    versionfinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-import { CartProvider } from "./components/CartContext"; // Asegúrate de importar CartProvider
+import CartWidget from "./components/CartWidget";
+import Checkout from "./components/Checkout";
+import Description from "./components/Description";
+import ItemDetail from "./components/ItemDetail";
+import ItemList from "./components/ItemList";
+import ItemQuantitySelector from "./components/ItemQuantitySelector";
+import AddItemButton from "./components/AddItemButton";
+import Brief from "./components/Brief";
+import ProfileCard from "./components/ProfileCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import ItemList from './ItemList';
 
 function App() {
     return (
-    <CartProvider> {/* Envuelve tu aplicación con CartProvider */}
-        <Router>
+    <Router>
         <div>
-            <NavBar />
-          <Routes> {/* Corrige Boutes a Routes */}
+        <NavBar />
+        <CartWidget />
+        <Routes>
             <Route path="/" element={<ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />} />
             <Route path="/category/:id" element={<ItemListContainer />} />
             <Route path="/item/:id" element={<ItemDetailContainer />} />
-          </Routes> {/* Corrige Boutes a Routes */}
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/description" element={<Description />} />
+            <Route path="/itemdetail" element={<ItemDetail />} />
+            <Route path="/itemlist" element={<ItemList />} />
+            <Route path="/itemquantityselector" element={<ItemQuantitySelector />} />
+            <Route path="/additembutton" element={<AddItemButton />} />
+            <Route path="/brief" element={<Brief />} />
+            <Route path="/profilecard" element={<ProfileCard />} />
+        </Routes>
         </div>
-        </Router>
-    </CartProvider>
+    </Router>
     );
 }
 
