b87412f2f7e401abea4b3083d84867e6c876bd31
Laionel2002 <lionel.a.gomez.g@gmail.com>
2025-01-29 00:30:34 -0300

    versionfinal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Asegúrate de tener un archivo CSS para los estilos
+import './index.css';; // Asegúrate de tener un archivo CSS para los estilos
 
 // Obtén una referencia al div con id root
 const el = document.getElementById('root');
